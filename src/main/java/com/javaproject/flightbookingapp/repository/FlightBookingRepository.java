208f75f04c7f8da0588b7f158819cda8c456e365
Saratu Aye Doma <saratudoma@gmail.com>
2024-01-23 12:39:45 +0100

    Domain file commit

--- src/main/java/com/javaproject/flightbookingapp/repository/FlightBookingRepository.java
+++
@@ -1,8 +0,0 @@
-//package com.javaproject.flightbookingapp.repository;
-//
-//import com.javaproject.flightbookingapp.domain.FlightBooking;
-//import org.springframework.data.jpa.repository.JpaRepository;
-//
-//public interface FlightBookingRepository extends JpaRepository<FlightBooking, Long> {
-//
-//}
