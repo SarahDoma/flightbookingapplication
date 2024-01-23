42dd7705a6e1eed0bb39cc9f22c2245d05553c44
Saratu Aye Doma <saratudoma@gmail.com>
2024-01-23 11:51:32 +0100

    Domain Commit

--- src/main/java/com/javaproject/flightbookingapp/domain/FlightBooking.java
+++ src/main/java/com/javaproject/flightbookingapp/domain/FlightBooking.java
@@ -1,26 +1,26 @@
-/*package com.javaproject.flightbookingapp.domain;
+package com.javaproject.flightbookingapp.domain;
 
 import jakarta.persistence.*;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.format.annotation.DateTimeFormat;
 
+import java.time.LocalDate;
 import java.util.Date;
-import java.util.List;
 
-@Entity(name="flight")
-
+@Entity(name = "flight")
 public class FlightBooking {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     private String passengerFirstName;
     private String passengerLastName;
-    private int passengerTelephone;
+    private String passengerTelephone;
     private String passengerEmail;
     private String flightType;
     private String airline;
-    private int flightPrice;
-    private Date bookingDate;
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private LocalDate bookingDate;
 
     @CreationTimestamp
     private Date created_date;
@@ -29,27 +29,32 @@ public class FlightBooking {
     private Date update_date;
 
     public FlightBooking() {
-
     }
 
     public String getPassengerFirstName() {
         return passengerFirstName;
     }
+
     public void setPassengerFirstName(String passengerFirstName) {
         this.passengerFirstName = passengerFirstName;
     }
+
     public String getPassengerLastName() {
         return passengerLastName;
     }
-    public void setLastName(String passengerLastName) {
+
+    public void setPassengerLastName(String passengerLastName) {
         this.passengerLastName = passengerLastName;
     }
-    public int getPassengerTelephone() {
+
+    public String getPassengerTelephone() {
         return passengerTelephone;
     }
-    public void setPassengerTelephone(int passengerTelephone) {
+
+    public void setPassengerTelephone(String passengerTelephone) {
         this.passengerTelephone = passengerTelephone;
     }
+
     public String getPassengerEmail() {
         return passengerEmail;
     }
@@ -57,6 +62,7 @@ public class FlightBooking {
     public void setPassengerEmail(String passengerEmail) {
         this.passengerEmail = passengerEmail;
     }
+
     public String getFlightType() {
         return flightType;
     }
@@ -72,39 +78,47 @@ public class FlightBooking {
     public void setAirline(String airline) {
         this.airline = airline;
     }
-
-    public Date setBookingDate() {
-        return bookingDate;
-    }
-
-    public int getFlightPrice() {
-        return flightPrice;
-    }
-
-    public void setFlightPrice(int flightPrice) {
-        this.flightPrice = flightPrice;
-    }
-
     public void setId(Long id) {
         this.id = id;
     }
-
     public Long getId() {
         return id;
     }
 
-     public FlightBooking(String passengerFirstName, String passengerLastName, int passengerTelephone, String passengerEmail, String flightType, String airline, Date bookingDate, int flightPrice, Long id, Date created_date, Date update_date) {
+     public FlightBooking(String passengerFirstName, String passengerLastName, String passengerTelephone, String passengerEmail, String flightType, String airline, LocalDate bookingDate, Long id, Date created_date, Date update_date) {
         this.passengerFirstName = passengerFirstName;
         this.passengerLastName = passengerLastName;
         this.passengerTelephone = passengerTelephone;
         this.passengerEmail = passengerEmail;
         this.flightType = flightType;
         this.airline = airline;
-        this.bookingDate = bookingDate;
-        this.flightPrice = flightPrice;
         this.id = id;
+        this.bookingDate = bookingDate;
         this.created_date = created_date;
         this.update_date = update_date;
     }
 
-}*/
+    public LocalDate getBookingDate() {
+        return bookingDate;
+    }
+
+    public void setBookingDate(LocalDate bookingDate) {
+        this.bookingDate = bookingDate;
+    }
+
+    public Date getCreated_date() {
+        return created_date;
+    }
+
+    public void setCreated_date(Date created_date) {
+        this.created_date = created_date;
+    }
+
+    public Date getUpdate_date() {
+        return update_date;
+    }
+
+    public void setUpdate_date(Date update_date) {
+        this.update_date = update_date;
+    }
+}
--- src/main/java/com/javaproject/flightbookingapp/services/FlightBookingServiceImpl.java
+++ src/main/java/com/javaproject/flightbookingapp/services/FlightBookingServiceImpl.java
@@ -1,62 +1,54 @@
-/*package com.javaproject.flightbookingapp.services;
-
-import com.javaproject.flightbookingapp.domain.FlightBooking;
-import com.javaproject.flightbookingapp.repository.FlightBookingRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
-import java.util.List;
-import java.util.Optional;
-
-@Service
-public class FlightBookingServiceImpl implements FlightBookingService {
-
-    @Autowired
-    FlightBookingRepository flightBookingRepository;
-
-    @Override
-    public FlightBooking saveFlight(FlightBooking flightBooking) {
-        return flightBookingRepository.save(flightBooking);
-    }
-
-    @Override
-    public FlightBooking getFlightById(long id) {
-        Optional<FlightBooking> flight = flightBookingRepository.findById(id);
-        FlightBooking flightIsAvailable = null;
-        if (flight.isPresent()) {
-            flightIsAvailable = flight.get();
-            return flightIsAvailable;
-        } else {
-            throw new RuntimeException("This flight is not available");
-        }
-    }
-
-    @Override
-    public List<FlightBooking> getAllFlights() {
-        return flightBookingRepository.findAll();
-    }
-
-    @Override
-    public FlightBooking updateFlightById(FlightBooking flightBooking) {
-        Optional<FlightBooking> optionalFlightBooking = flightBookingRepository.findById(flightBooking.getId());
-        if (optionalFlightBooking.isPresent()) {
-            FlightBooking updateFlightById = optionalFlightBooking.get();
-            updateFlightById.setPassengerFirstName(flightBooking.getPassengerFirstName());
-            updateFlightById.setLastName(flightBooking.getPassengerLastName());
-            updateFlightById.setPassengerTelephone(flightBooking.getPassengerTelephone());
-            updateFlightById.setPassengerEmail(flightBooking.getPassengerEmail());
-            updateFlightById.setFlightType(flightBooking.getFlightType());
-            updateFlightById.setAirline(flightBooking.getAirline());
-            flightBookingRepository.save(updateFlightById);
-
-            return updateFlightById;
-        } else {
-            throw new RuntimeException("This flight is currently not available");
-        }
-    }
-
-    @Override
-    public void deleteFlightById(long id) {
-        flightBookingRepository.deleteById(id);
-    }
-}*/
+//package com.javaproject.flightbookingapp.services;
+//
+//import com.javaproject.flightbookingapp.domain.FlightBooking;
+//import com.javaproject.flightbookingapp.repository.FlightBookingRepository;
+//import jakarta.persistence.EntityNotFoundException;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.stereotype.Service;
+//
+//import java.util.NoSuchElementException;
+//import java.util.List;
+//import java.util.Optional;
+//
+//@Service
+//public class FlightBookingServiceImpl implements FlightBookingService {
+//
+//    @Autowired
+//    FlightBookingRepository flightBookingRepository;
+//
+//    @Override
+//    public FlightBooking saveFlight(FlightBooking flightBooking) {
+//        return flightBookingRepository.save(flightBooking);
+//    }
+//
+//    @Override
+//    public FlightBooking getFlightById(long id) {
+//        Optional<FlightBooking> flight = flightBookingRepository.findById(id);
+//        return flight.orElseThrow(() -> new NoSuchElementException("Flight not found with id: " + id));
+//    }
+//
+//    @Override
+//    public List<FlightBooking> getAllFlights() {
+//        return flightBookingRepository.findAll();
+//    }
+//
+//    @Override
+//    public FlightBooking updateFlightById(FlightBooking flightBooking) {
+//        Optional<FlightBooking> optionalFlightBooking = flightBookingRepository.findById(flightBooking.getId());
+//        return optionalFlightBooking.map(updateFlightById -> {
+//            updateFlightById.setPassengerFirstName(flightBooking.getPassengerFirstName());
+//            updateFlightById.setPassengerLastName(flightBooking.getPassengerLastName());
+//            updateFlightById.setPassengerTelephone(flightBooking.getPassengerTelephone());
+//            updateFlightById.setPassengerEmail(flightBooking.getPassengerEmail());
+//            updateFlightById.setFlightType(flightBooking.getFlightType());
+//            updateFlightById.setAirline(flightBooking.getAirline());
+//            updateFlightById.setAirline(String.valueOf(flightBooking.getBookingDate()));
+//            return flightBookingRepository.save(updateFlightById);
+//        }).orElseThrow(() -> new EntityNotFoundException("Flight not found with id: " + flightBooking.getId()));
+//    }
+//
+//    @Override
+//    public void deleteFlight(long id) {
+//        flightBookingRepository.deleteById(id);
+//    }
+//}
